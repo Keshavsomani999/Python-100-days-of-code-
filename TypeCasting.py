bf4b16a95b48674dc9be8c0bb9c2bd52616554af
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2022-12-06 22:23:45 +0530

    Day - 8

---
+++ TypeCasting.py
@@ -0,0 +1,20 @@
+# output
+# 2
+# <class 'float'>
+# <class 'int'>
+# 9.9
+# <class 'float'>
+
+
+a="1"
+b="1"
+
+print(int(a)+int(b))
+
+# Implicit TypeCasting
+c=1.9
+d=8
+print(type(c))
+print(type(d))
+print(c+d)
+print(type(c+d))
