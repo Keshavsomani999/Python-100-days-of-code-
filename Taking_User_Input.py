06ac5e67889fd4dbf7a3447ced05d051f734e670
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2022-12-07 20:24:04 +0530

    Day - 9

---
+++ Taking_User_Input.py
@@ -0,0 +1,22 @@
+# Output
+# Enter Your Name :Keshav
+# My name is : Keshav
+# Enter First Number : 5
+# Enter Second Number : 10
+# 15
+# -5
+# 50
+# 0.5
+
+
+a = input("Enter Your Name :")
+print("My name is :", a)
+
+b = int(input("Enter First Number : "))# We type casting b & c
+c = int(input("Enter Second Number : "))
+
+
+print(b+c) 
+print(b-c)
+print(b*c)
+print(b/c)
