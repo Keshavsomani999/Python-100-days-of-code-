cdfb67b9de0182e717f6c00d832749acefc97b71
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2022-12-08 22:01:47 +0530

    Day - 10

---
+++ Strings.py
@@ -0,0 +1,24 @@
+name = "Keshav"
+friend = "Rohan"
+anotherfriend = "Arpit"
+apple = 'He said, " I want to eat an apple'
+
+
+# MultiLine String
+Orange = '''
+He Said ,
+It is multiline
+String
+'''
+
+print("Hello " + name)
+print(apple)
+print(Orange)
+
+print(name[0]) #String Indexing
+print(name[2])
+
+print("")
+#String Loop
+for i in name:
+    print(i)
