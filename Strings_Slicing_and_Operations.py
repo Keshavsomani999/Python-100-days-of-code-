c66c9fa3047190016f358c52090962f788822d16
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2022-12-09 22:53:11 +0530

    Day - 11

---
+++ Strings_Slicing_and_Operations.py
@@ -0,0 +1,17 @@
+names = "Keshav"
+print(len(names))
+print(names[0:5])
+print(names[1:5]) 
+print(names[0:-3])
+print(names[-3:-1]) #print(names[len(names)-3:len(names)-1])
+
+# nm = "Harry"
+# print(nm[-4:-2])
+'''
+nm[len(nm)-4:len(nm)-2]
+nm[5-4:5-2]
+nm[1:3]
+
+
+ANS : ar
+'''
