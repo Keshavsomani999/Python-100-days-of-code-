8a80b9c7003428a77e8a2ba3f2a2b4adb6e4dfd4
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2022-12-21 13:43:00 +0530

    Day 12

---
+++ String Methods in Python.py
@@ -0,0 +1,29 @@
+a = "Keshav Somani Mayank"
+b = "keshav!!!!"
+Heading = "introduction to Python"
+# Strings are immutable
+
+
+print(a.upper()) #That method make new string can not change old string
+print(a.lower())
+print(b.strip("!"))# remove ! FROM STRING
+print(a.replace("Keshav","mayank"))
+
+print(a.split(" "))
+
+print(Heading.capitalize())
+
+print(Heading.center(50))
+
+print(a.count("Keshav"))
+
+print(a.find("Somani"))
+
+print(a.isalnum())
+
+print(a.istitle())
+
+
+
+
+
