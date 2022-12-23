a1b3d20477755ed4553aa7426fc4d1709235e47a
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2022-12-23 21:56:32 +0530

    Day -16

---
+++ For_Loop.py
@@ -0,0 +1,19 @@
+# Name = 'Keshav'
+# for i in Name:
+#     if(i == "e"):
+#         print("This is something special !")
+#     print(i)
+    
+# color = ["Red","Green","Blue","Yellow"]
+
+# for color in color:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+
+# for k in range(5):
+#     print(k)
+
+for k in range(1,9,2):     # third parameter is increment
+    print(k)
