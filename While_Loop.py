c94745fdc8f2298871cc40f4d0b826bb6dcad7dd
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2022-12-24 09:17:23 +0530

    Day -17

---
+++ While_Loop.py
@@ -0,0 +1,14 @@
+# i = int(input("Enter the number"));
+
+
+# while(i<3):
+#     i = int(input("Enter the number"));
+#     print(i)
+#     i = i+1
+
+
+
+count = 5
+while(count>0):
+    print(count)
+    count = count-1
