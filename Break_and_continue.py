b11bba23c760100ec10007498d56781eb3b7bf49
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2022-12-24 22:22:16 +0530

    Day - 18

---
+++ Break_and_continue.py
@@ -0,0 +1,19 @@
+# break
+
+# for i in range(12):
+#     if(i==10):
+#         break
+#     print("5 X", i+1, "=" ,5 * (i+1))
+# print("loop ko chodkar nikal gaya")
+
+
+# continue
+
+for i in range(12):
+    if(i==5):
+        print("skip the iteration")
+        continue
+    print("5 X", i+1, "=" ,5 * (i+1))
+
+
+
