2fd5277c921135daa62f86525970cf4d93ca0644
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2022-12-29 22:20:53 +0530

    Day - 22

---
+++ Lists_in_Python.py
@@ -0,0 +1,27 @@
+marks = [3,5,6,"harry"] # List have different datatypes we will access by indexing
+# print(marks[0])
+# print(marks[1])
+# print(marks[2])
+# print(marks[3])
+
+# print(marks[-3])
+
+# if 9 in marks:
+#     print("yes")
+# else:
+#     print("no")
+
+
+# if "av" in "Keshav":
+#     print("yes")
+# else:
+#     print("no")
+
+# print(marks[1:-1])
+# print(marks[0:9:2])
+
+# lst = [i*i for i in range(8)]
+# print(lst)
+
+lst = [i for i in range(12) if i%2==0]
+print(lst)
