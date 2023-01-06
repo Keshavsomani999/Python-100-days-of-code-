9dec513af1ead1e31959f88ab7ea5aeaad7f8c47
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2023-01-06 20:44:55 +0530

    Day - 28

---
+++ f-strings.py
@@ -0,0 +1,13 @@
+letter = "Hey my name is {} and I am from {}"
+country = "India"
+name = "Keshav"
+
+print(letter.format(name,country))
+
+print(f"Hey my name is {name} and I am from {country}")
+
+price = 59.99
+
+txt = f"for only {price:2f}"
+
+print(txt)
