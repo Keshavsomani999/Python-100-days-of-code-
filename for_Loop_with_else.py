e00f49ea36258fcb8ef457b364281d3554c251c5
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2023-01-10 18:50:46 +0530

    Day - 35

---
+++ for_Loop_with_else.py
@@ -0,0 +1,33 @@
+# for i in range(5):
+#     print (i)
+
+# else:
+#     print("sorry no i")
+
+# output :-
+# 0
+# 1
+# 2
+# 3
+# 4
+# sorry no i
+
+# for i in []:
+#     print (i)
+
+# else:
+#     print("sorry no i")   #sorry no i
+
+for i in range(5):
+    print(i)
+    if i==4:
+        break
+else:
+    print("sorry no i")  
+
+#output 
+# 0 
+# 1
+# 2
+# 3
+# 4
