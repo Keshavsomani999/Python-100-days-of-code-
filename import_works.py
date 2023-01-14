9c85d2d1fbfe00a453f81c3239064a288ff00313
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2023-01-14 19:08:58 +0530

    Day - 44

---
+++ import_works.py
@@ -0,0 +1,13 @@
+import math 
+from keshav import welcome,keshav 
+# import math as a
+
+# math.floor(4.2343)
+
+# result = a.sqrt(9)
+# print(result)   #output 3.0
+
+# print(dir(math)) # it show all function inside math
+
+welcome()
+print(keshav)
