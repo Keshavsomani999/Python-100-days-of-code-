c3fa19cdbe5c48530036379b1e353770ecdc4347
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2023-01-15 19:08:00 +0530

    Day - 45

--- __name__.py
+++ __name__.py
@@ -1,3 +1,12 @@
 import keshav
 
 keshav.welcome();
+
+
+# other file code 
+
+def welcome():
+    print("Hey you are welcome my friend")
+
+if __name__=="__main__":               #it only run when this file is run
+    welcome()
