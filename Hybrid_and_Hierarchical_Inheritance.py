67f6e53d46d71953cdea93550ebcac18e7db1b08
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2023-02-16 19:14:51 +0530

    Day - 81

---
+++ Hybrid_and_Hierarchical_Inheritance.py
@@ -0,0 +1,37 @@
+# Hybrid
+
+class BaseClass:
+    pass
+
+class Derived1(BaseClass):
+    pass
+
+class Derived2(BaseClass):
+    pass
+
+class Derived3(Derived1,Derived2):
+    pass
+
+
+
+# Hierarchical inInheritance
+
+class BaseClasss:
+    pass
+
+class D1(BaseClass):
+    pass
+
+
+class D2(BaseClass):
+    pass
+
+class D3(D1):
+    pass
+
+class D4(D1):
+    pass
+
+
+
+    
