d4ef43d06b19e905d51de27402777d748344bb0b
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2023-02-20 18:21:06 +0530

    Day - 84

---
+++ Time_Module.py
@@ -0,0 +1,25 @@
+import time
+# def usingWhile():
+#     i =0
+#     while i<5000:
+#         i=i+1
+#         print(i)
+
+# def usingFor():
+#     for i in range(5000):
+#         print(i)
+
+
+# init = time.time()
+# usingWhile()
+# t1 = time.time() - init
+# init = time.time()
+
+# usingFor()
+# print(time.time() - init)
+# print(t1)
+
+
+print(4)
+time.sleep(3)
+print("This is wait printed after 3 sec")
