883c40cd9d8d24a916e3901c5bb46b66adafb981
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2023-02-22 19:05:38 +0530

    Day - 86

---
+++ Walrus_Operator.py
@@ -0,0 +1,13 @@
+
+# print(a:=False)
+
+# numbers = [1,2,4,5,6,7]
+
+# while(n := len(numbers))>0:
+#     print(numbers.pop())
+
+
+foods = list()
+
+while(food:=input("what food do you like? :")) != "quit":
+    foods.append(food)
